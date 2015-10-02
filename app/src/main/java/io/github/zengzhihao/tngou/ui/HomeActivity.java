d45c572a669e41bd28b7200feb020ca798cc769f
Kela King <zengzhihao2015@163.com>
2015-10-02 14:13:54 +0800

    初始化 项目依赖

---
+++ app/src/main/java/io/github/zengzhihao/tngou/ui/HomeActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 zengzhihao.github.io. All rights reserved.
+ * Support: http://zengzhihao.github.io
+ */
+
+package io.github.zengzhihao.tngou.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Kela.King
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
